d60d3d792a7ad1421e089ac254a0ad442ad0364a
Rick <rick1130_iphone@icloud.com>
2018-03-13 23:41:29 +0900

    Add MouseSynchronizeObjectScript and SingletonMonoBehaviour

---
+++ Assets/Scritps/Inputs/MouseSynchronizeObjectScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts.Inputs
+{
+    public class MouseSynchronizeObjectScript : MonoBehaviour
+    {
+        // 位置座標
+        private Vector3 position;
+        // スクリーン座標をワールド座標に変換した位置座標
+        private Vector3 screenToWorldPointPosition;
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Vector3でマウス位置座標を取得する
+            position = Input.mousePosition;
+            // Z軸修正
+            position.z = 10f;
+            // マウス位置座標をスクリーン座標からワールド座標に変換する
+            screenToWorldPointPosition = UnityEngine.Camera.main.ScreenToWorldPoint(position);
+            // ワールド座標に変換されたマウス座標を代入
+            gameObject.transform.position = screenToWorldPointPosition;
+        }
+    }   
+}
